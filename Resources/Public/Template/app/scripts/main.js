d4d3fca93314716fd2f5508644f5fc0c8ae90f2b
Martin Pfister <martin.pfister@students.fhnw.ch>
2015-10-20 11:46:32 +0200

    added frontend directories

---
+++ Resources/Public/Template/app/scripts/main.js
@@ -0,0 +1,87 @@
+jQuery(document).ready(function($) {
+    var secondaryNav            = $('.cd-secondary-nav'),
+        secondaryNavTopPosition = secondaryNav.offset().top,
+        taglineOffesetTop       = $('#cd-intro').height() + parseInt($('#logo').css('paddingTop').replace('px', '')),
+        contentSections         = $('.cd-section'),
+        logo                    = $('#home');
+
+    $(window).on('scroll', function() {
+        //on desktop - assign a position fixed to logo and action button and move them outside the viewport
+        if( $(window).scrollTop() > taglineOffesetTop) {
+            logo.addClass('is-hidden');
+        }
+        //on desktop - fix secondary navigation on scrolling
+        if ($(window).scrollTop() > secondaryNavTopPosition) {
+            //fix secondary navigation
+            secondaryNav.addClass('is-fixed');
+            //push the .cd-main-content giving it a top-margin
+            $('.cd-main-content').addClass('has-top-margin');
+            setTimeout(function() {
+                logo.addClass('slide-in');
+            }, 50);
+        } else {
+            secondaryNav.removeClass('is-fixed');
+            $('.cd-main-content').removeClass('has-top-margin');
+            setTimeout(function() {
+                secondaryNav.removeClass('animate-children');
+                logo.removeClass('slide-in');
+            }, 50);
+        }
+
+        //on desktop - update the active link in the secondary fixed navigation
+        updateSecondaryNavigation();
+    });
+
+    function updateSecondaryNavigation() {
+        contentSections.each(function() {
+            var actual = $(this),
+                actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
+                actualAnchor = secondaryNav.find('a[href="#' + actual.attr('id') + '"]');
+            if ((actual.offset().top - secondaryNav.height() <= $(window).scrollTop()) && (actual.offset().top + actualHeight - secondaryNav.height() > $(window).scrollTop())) {
+                actualAnchor.addClass('active');
+            } else {
+                actualAnchor.removeClass('active');
+            }
+        });
+    }
+
+    //on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
+    $('.cd-secondary-nav-trigger').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('menu-is-open');
+        secondaryNav.find('ul').toggleClass('is-visible');
+    });
+
+    //smooth scrolling when clicking on the secondary navigation items
+    secondaryNav.find('ul a').on('click', function(event) {
+        event.preventDefault();
+        var target = $(this.hash);
+        $('body,html').animate({
+            'scrollTop': target.offset().top - secondaryNav.height() + 1
+        }, 400);
+        //on mobile - close secondary navigation
+        $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
+        secondaryNav.find('ul').removeClass('is-visible');
+    });
+
+    $('#foward').on('click', function(event) {
+        event.preventDefault();
+        $('body,html').animate({
+            'scrollTop': $('#motto').offset().top
+        }, 700);
+    });
+
+    $('#home').on('click', function(event) {
+        event.preventDefault();
+        var height = $('#motto').offset().top - 70 + 1;
+        console.log(height);
+        $('body,html').animate({
+            'scrollTop':height
+        }, 700);
+    });
+
+    //on mobile - open/close primary navigation clicking/tapping the menu icon
+    $('.cd-primary-nav').on('click', function(event) {
+        if ($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+    });
+});
