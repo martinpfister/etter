fe8632ed41df07e2872e0b3d81ad9896bb92d4ce
Martin Pfister <martin.pfister@students.fhnw.ch>
2015-11-02 13:31:23 +0100

    added Gridelement, removed unused images

--- Resources/Private/TypoScript/setup-production.ts
+++ Resources/Private/TypoScript/setup-production.ts
@@ -4,7 +4,7 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/config_language.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/config.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/page.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Page/page_socialmediametatags.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Page/page_socialmediametatags.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/page_felayoutrendering.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/page_jscssincludes.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Page/page_icons.ts">
@@ -19,12 +19,12 @@
 # Load typoscript template objects
 # **********************************************************
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/menu.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/menu-foundation-topbar.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/menu-language.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/menu-foundation-topbar.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/menu-language.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/contentloader.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/contentloader-slide.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/currentyear.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/searchbox.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/contentloader-slide.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/currentyear.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/temp/searchbox.ts">
 
 # **********************************************************
 # Load objects to be rendered
@@ -32,22 +32,17 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/contentloader.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/logo.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/menu.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/menu-language.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/currentyear.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/skiplinks.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/menu-language.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/currentyear.ts">
+#<INCLUDE_TYPOSCRIPT: source="FILE:./Content/lib/skiplinks.ts">
 
 # **********************************************************
 # T3 content objects render config
 # **********************************************************
 <INCLUDE_TYPOSCRIPT: source="FILE:./Content/tt_content.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:./Content/mailform.ts">
 
 # **********************************************************
 # Grid elements render config
 # **********************************************************
-<INCLUDE_TYPOSCRIPT: source="FILE:./GridElements/Slider.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:./GridElements/TwoColumns.ts">
-
-
-
-
+<INCLUDE_TYPOSCRIPT: source="FILE:./GridElements/TwoColumnsContact.ts">
